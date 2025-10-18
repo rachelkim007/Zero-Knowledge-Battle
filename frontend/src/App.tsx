e4dba38ea21c0a6da8912b2be60273ae249be362
devsophiame <BoudewynsBowl286@hotmail.com>
2025-10-19 01:02:45 +0800

    great start

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { config } from './config/wagmi';
+import { BattleApp } from './components/BattleApp';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider locale="en">
+          <BattleApp />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default App
