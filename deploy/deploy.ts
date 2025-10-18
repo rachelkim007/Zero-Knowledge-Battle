b02e57f99e096163978bb359da659d516634b826
rachelkim007 <GilleyFransisco69@hotmail.com>
2025-10-19 00:47:57 +0800

    update deploy.js

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,34 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const goldDeployment = await deploy("ConfidentialGold", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const battleDeployment = await deploy("ZeroKnowledgeBattle", {
+    from: deployer,
+    log: true,
+    args: [goldDeployment.address],
+  });
+
+  const existingBattleContract = await hre.deployments.read("ConfidentialGold", "battleContract");
+
+  if (existingBattleContract !== battleDeployment.address) {
+    await hre.deployments.execute(
+      "ConfidentialGold",
+      {
+        from: deployer,
+        log: true,
+      },
+      "setBattleContract",
+      battleDeployment.address,
+    );
+  }
+
+  console.log("ConfidentialGold contract:", goldDeployment.address);
+  console.log("ZeroKnowledgeBattle contract:", battleDeployment.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_zero_knowledge_battle";
+func.tags = ["ZeroKnowledgeBattle"];
